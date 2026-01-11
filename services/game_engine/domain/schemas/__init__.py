f34b77ed2a9dd3318db42fd6a45c639d7832cbd9
andreysiniy <kakavvmm@gmail.com>
2026-01-11 07:24:36 +0300

    Add RussianRouletteAction import and infrastructure models

--- services/game_engine/domain/schemas/__init__.py
+++ services/game_engine/domain/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .fishing import FishRequest, FishResponse
 from .rpg import ItemDTO, PlayerStateDTO
-from .actions import TimeoutAction, StreamElementsPointsAction, RobberyAction, GameAction, BaseAction
+from .actions import TimeoutAction, StreamElementsPointsAction, RobberyAction, GameAction, BaseAction, RussianRouletteAction
 from .common import RewardType, Rarity
 from .admin import ChannelCreateDTO, ChannelUpdateDTO, ChannelResponseDTO, RewardPoolUpdateDTO, RewardPoolResponseDTO
