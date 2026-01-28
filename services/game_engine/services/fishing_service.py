0a9e6f3ca971a789807173e10d1e06c97192be60
andreysiniy <kakavvmm@gmail.com>
2026-01-29 01:42:57 +0300

    Added Robbery reward handling to fish service

--- services/game_engine/services/fishing_service.py
+++ services/game_engine/services/fishing_service.py
@@ -1,7 +1,12 @@
 from infrastructure.repositories import UserRepository, ConfigRepository
+from infrastructure.models import UserProgress
+
+from core.action_types import ActionType
 
 from services.fishing.engine import FishingEngine
 from services.fishing.presenter import FishingPresenter
+
+from domain.schemas.fishing import RobberyResultDTO
 
 class FishingService:
     def __init__(self, user_repo: UserRepository, config_repo: ConfigRepository):
@@ -26,6 +31,9 @@ class FishingService:
             custom_params=user.channel.config.get("custom_params", {})
         )
 
+        if result.loot.get("type") == ActionType.ROBBERY:
+            result.robbery_result = self._handle_robbery(result.loot, user)
+
         user.xp += result.xp_gained
         user.total_fish_stat += 1
         
@@ -44,3 +52,29 @@ class FishingService:
         response = self.presenter.build_response(user, result)
         
         return response
+    
+    def _handle_robbery(self, loot: dict, user: UserProgress) -> RobberyResultDTO:
+        lookup_range = loot.get("range", 3)
+        channel_config = user.channel.config or {}
+
+        victim = self.user_repo.get_rich_victim(
+            channel_id=user.channel.id,
+            attacker_id=user.id,
+            lookup_range=lookup_range
+        )        
+
+        robbery_result = self.engine.calculate_mass_robbery(
+            attacker=user,
+            victim=victim,
+            channel_config=channel_config,
+            catch=loot
+        )
+
+        if robbery_result.is_success:
+            user.current_mass += robbery_result.amount_stolen
+            user.total_mass_stat += robbery_result.amount_stolen
+            victim.current_mass -= robbery_result.amount_stolen
+            victim.total_mass_stat -= robbery_result.amount_stolen
+            self.user_repo.save_progress(victim)
+        
+        return robbery_result
